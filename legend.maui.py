3b42a73859a230297be82023ad435e067d88458a
KHOWI <william.kho@student.onslow.school.nz>
2019-07-09 18:52:43 +1200

    V0.04 - Movement Checking
    + Movement Checking now functions properly (Will now prevent the player from leaving the bounds of the map)

--- legend.maui.py
+++ legend.maui.py
@@ -1,6 +1,6 @@
 ##
 #  The Legend of Maui
-#  V0.03
+#  V0.04
 
 
 def map_generator_1():
@@ -10,39 +10,57 @@ def map_generator_1():
     return STAGE_1
 
 
-def movement_processor(player):
+def movement_processor(stage,player):
     """Takes input from players to move"""
-    movement = input().lower()
     player_x = player[0]
     player_y = player[1]
     print(player_x,player_y)
-    
-    if movement != "up" and movement and "down" and movement !="right" and movement !="left":
-        print("broke")
-    elif movement == "up":
-        player_x = player_x + 1
-    elif movement == "down":
-        player_x = player_x - 1
-    elif movement == "left":
-        player_y = player_y + 1
-    elif movement == "right":
-        player_y = player_y - 1
 
-    player = [player_x,player_y]
-    return player
+    valid = False
+    while not valid:
+        movement = input("Enter a movement").lower()
+        if (movement != "up"
+            and movement != "down"
+                and movement !="right"
+                    and movement !="left"):
+            print("Please enter a valid command.")
+        elif movement == "up":
+            player_x = player_x + 1
+        elif movement == "down":
+            player_x = player_x - 1
+        elif movement == "left":
+            player_y = player_y + 1
+        elif movement == "right":
+            player_y = player_y - 1
+
+        player_new = [player_x,player_y]
+        valid = movement_checker(stage,player_new)
+        #  reset t
+        if not valid:
+            player_x = player[0]
+            player_y = player[1]
+    
+    return player_new
 
 def movement_checker(stage,player_new):
     """Checks to see whether movement is valid"""
     if player_new[0] == 0:
-        print("no")
+        valid = False
+        print("No")
     elif player_new[1] == 0:
-        print("bad")
+        valid = False
+        print("No")
     elif player_new[0] == stage[0]:
-        print("NO")
+        valid = False
+        print("No")
     elif player_new[0] == stage[1]:
-        print("BAD")
+        valid = False
+        print("No")
     else:
-        return player_new
+        valid = True
+        print("Yes")
+    return valid
+    
         
 
 def map_displayer(player):
@@ -58,7 +76,7 @@ def main():
     playing = True
     while playing:
         map_displayer(player)
-        player = movement_checker(stage,movement_processor(player))
+        player = movement_processor(stage,player)
     pass
 
 main()
