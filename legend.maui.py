871464246170d9fd131ec9efe689743f14c75548
KHOWI <william.kho@student.onslow.school.nz>
2019-07-15 17:47:46 +1200

    v0.09 - Special Tile Processing + PEP 8
    + Added framework for special condition processing
        + Currently only has end
             + Victory can now be achieved!
    
    ~ Tidied up code according to PEP 8 Convention
    + Added addition commenting for where different game phases would occur

--- legend.maui.py
+++ legend.maui.py
@@ -1,20 +1,32 @@
 ##
 #  The Legend of Maui
-#  V0.08
+#  V0.09
 ## =====-----------  ----------=====
 ## =====----------- Map Generation ----------=====
 def map_generator_1(option):
     """Generates the stage should go in format [Stage Size] [Player Starting Position]"""
-    STAGE_1 = ([5,5],[1,1]) 
-    STAGE_1_TILES = {
+    # Stage Size + Player Starting Position
+    STAGE_1 = ([5,5], [1,1])
+
+    # Non-Ocean tiles
+    STAGE_1_TILES = {         
         "1,2":"rock",
         "1,3":"mountain",
         "5,5":"end"
-        }
+    }
+
+    # Special Tiles that trigger an event
+    STAGE_1_SPECIAL = {       
+        "5,5":"end"
+    }
+
+    # Decide what data to return
     if option == "stage":
         return STAGE_1
     elif option == "tiles":
         return STAGE_1_TILES
+    elif option == "special":
+        return STAGE_1_SPECIAL
     else:
         print("Something Broke! map_generator_1")
 
@@ -27,12 +39,14 @@ def tile_set():
         ,"player":"X"
         ,"end":"E"
         }
+
     return TILES
 
 ## =====----------- Turn Processing ----------=====
 
 #  ------------------ Movement -----------------
-def movement_processor(stage,player,stage_tiles):
+def movement_processor(stage, player,
+                       stage_tiles):
     """Takes input from players to move"""
     player_x = player[0]
     player_y = player[1]
@@ -40,11 +54,14 @@ def movement_processor(stage,player,stage_tiles):
     valid = False
     while not valid:
         movement = input("Enter a movement:").lower()
+        # Detect if the movement command is not valid
         if (movement != "up"
             and movement != "down"
                 and movement !="right"
                     and movement !="left"):
             print("Please enter a valid command.")
+
+        # Change player's co-ordinates appropriately
         elif movement == "down":
             player_y = player_y - 1
         elif movement == "up":
@@ -54,21 +71,22 @@ def movement_processor(stage,player,stage_tiles):
         elif movement == "left":
             player_x = player_x - 1
 
-        #  Movement Validation starts here
-        player_new = [player_x,player_y]
-        valid = boundary_checker(stage,player_new)
+        # Movement validation
+        player_new = [player_x, player_y]
+        valid = boundary_checker(stage, player_new)
         if valid:
-            valid = tile_checker(stage_tiles,player_new)
+            valid = tile_checker(stage_tiles, player_new)
 
-        #  Reset the x and y
+        # Reset the x and y
         if not valid:
             player_x = player[0]
             player_y = player[1]
     
     return player_new
 
-def boundary_checker(stage,player_new):
-    """Checks to see whether movement is valid"""
+def boundary_checker(stage, player_new):
+    """Checks to see whether movement is would take the player out of bounds"""
+    # Go through each possible direction a player can travel
     if player_new[0] == 0:
         valid = False
         print("You can't leave the map!")
@@ -81,65 +99,92 @@ def boundary_checker(stage,player_new):
     elif player_new[1] > stage[1]:
         valid = False
         print("You can't leave the map!")
+    # Flag validity if player still within bounds of map
     else:
         valid = True
+
     return valid
 
-def tile_checker(stage_tiles,player_new):
+def tile_checker(stage_tiles,
+                 player_new):
     """Checks to see what tile the player would be on"""
-    tile = stage_tiles.get("{0},{1}".format(player_new[0],player_new[1]),"ocean")
+    tile = stage_tiles.get("{0},{1}".format(player_new[0], player_new[1]), "ocean")
+    #  Check each possible terrain
     if tile == "rock" or tile == "mountain":
         valid = False
         print("You can't sail into a {}!".format(tile))
     else:
         valid = True
+
     return valid
 
 
-def special_condition_checker(stage_tiles,player_new):
+def special_condition_checker(special_tiles,
+                              player_new):
     """Checks to see whether there is a special condition triggered by the player"""
-    tile = stage_tiles.get("{0},{1}".format(player_new[0],player_new[1]),"ocean")
+    tile = special_tiles.get("{0},{1}".format(player_new[0], player_new[1]), None)
+    if tile == None: # If theres no special condition on that tile
+        return
+    elif tile == "end":
+        return tile
+
+    
     
 ## =====----------- UI Elements ----------=====
 
 #  ------------------ Map Gen -----------------
-def map_displayer(stage,player,stage_tiles,TILES):
+def map_displayer(stage, player,
+                  stage_tiles, TILES):
     """Displays the map in the console"""
-    print("=============================================")
+    print("=============================================")  # Hard seperation to show that a new turn has begun
+    # Setup variables
     x = 1
     y = stage[1]
     player_x = player[0]
     player_y = player[1]
+
     while y > 0:
         while x < stage[0]+1:
             if x == player_x and y == player_y:
-                print(TILES.get("player","X"), end='')
-            elif "{0},{1}".format(x,y) in stage_tiles:
-                tile = stage_tiles.get("{0},{1}".format(x,y),"ocean")
-                print(TILES[tile],end='')
+                print(TILES.get("player", "X"), end='')
+            elif "{0},{1}".format(x, y) in stage_tiles:
+                tile = stage_tiles.get("{0},{1}".format(x, y), "ocean")
+                print(TILES[tile], end='')
             else:
                 print('O', end='')
             x += 1
         print("")
-        y-=1
-        x= 1
+        y -= 1
+        x = 1
             
     
     print(player)
 
 def main():
     """Main Routine"""
+    # Setup values to be used in current stage
     stage = map_generator_1("stage")
     player = stage[1]
     stage = stage[0]
     stage_tiles = map_generator_1("tiles")
-    
+    special = map_generator_1("special")                              
     TILES = tile_set()
 
+    # Menu
+
+    # Intro
+    
+    # Game Start
     playing = True
     while playing:
-        map_displayer(stage,player,stage_tiles,TILES)
-        player = movement_processor(stage,player,stage_tiles)
-    pass
+        map_displayer(stage,player, stage_tiles, TILES)
+        # Print status
+        # Ask player for command
+        player = movement_processor(stage, player, stage_tiles)
+        update = special_condition_checker(special, player)
+        if update == "end":
+            playing = False
+        # Perform post turn actions
+    print("Thanks for playing")
 
 main()
